ce69d8a1a6e90827dc373c04f89572300164302d
Josh Gummersall <josh.gummersall@moz.com>
2016-02-13 09:28:01 -0800

    Use single script for checking whitelist/blacklist
    There's no need to separate these two routines since this code now uses
    early returns to idicate whitelist/blacklist status.

--- lua/check_blacklist.lua
+++
@@ -1,8 +0,0 @@
--- Check the blacklist set for the given key
-for _, key in ipairs(KEYS) do
-    local is_set_member = redis.call('SISMEMBER', blacklist_key, key)
-    is_set_member = tonumber(is_set_member)
-    if is_set_member > 0 then
-        return 2
-    end
-end
--- lua/check_whitelist.lua
+++
@@ -1,8 +0,0 @@
--- Check the whitelist set for the given key
-for _, key in ipairs(KEYS) do
-    local is_set_member = redis.call('SISMEMBER', whitelist_key, key)
-    is_set_member = tonumber(is_set_member)
-    if is_set_member > 0 then
-        return 0
-    end
-end
