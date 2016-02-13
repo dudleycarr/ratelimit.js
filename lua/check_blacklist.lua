2a98149c50562ea73eb79d21305718af94cc1bbe
Josh Gummersall <josh.gummersall@moz.com>
2016-02-13 09:21:05 -0800

    Simplify whitelist/blacklist lua code
    Well, TIL (yesterday technically) that you can just include early
    returns in Redis Lua code. All the code is more or less concatenated, so
    early returns will immediately halt execution of the remaining bits of
    code. The if checks previously used are unnecessary.

--- lua/check_blacklist.lua
+++ lua/check_blacklist.lua
@@ -1,12 +1,8 @@
--- Skip if we're already whitelisted
-if not is_whitelisted then
-    -- Check the blacklist set for the given key
-    for _, key in ipairs(KEYS) do
-        local is_set_member = redis.call('SISMEMBER', blacklist_key, key)
-        is_set_member = tonumber(is_set_member)
-        if is_set_member > 0 then
-            is_blacklisted = true
-            break
-        end
+-- Check the blacklist set for the given key
+for _, key in ipairs(KEYS) do
+    local is_set_member = redis.call('SISMEMBER', blacklist_key, key)
+    is_set_member = tonumber(is_set_member)
+    if is_set_member > 0 then
+        return 2
     end
 end
--- lua/check_whitelist.lua
+++ lua/check_whitelist.lua
@@ -3,7 +3,6 @@ for _, key in ipairs(KEYS) do
     local is_set_member = redis.call('SISMEMBER', whitelist_key, key)
     is_set_member = tonumber(is_set_member)
     if is_set_member > 0 then
-        is_whitelisted = true
-        break
+        return 0
     end
 end
